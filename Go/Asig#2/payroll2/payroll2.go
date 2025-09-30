ba260a010f57cc65cd2645a3146eb6a331e42673
Franz <franz.zbinden7@gmail.com>
2025-09-30 18:19:14 -0400

    Add payroll2.go to create and display data for a slice of five different employee types and their weekly salaries.

---
+++ Go/Asig#2/payroll2/payroll2.go
@@ -0,0 +1,8 @@
+/*
+ * File: payroll2.go
+ * Author: Franz Zbinden Garcia
+ * Course: COTI 4039-VH1
+ * Date: 09/30/2025
+ * Purpose: This program creates a slice of five employees of different types
+ *          and displays their data and weekly salary.
+ */
