02ac3c138fcf8c8af10a0fbc2a6db7fd988e19e9
Franz <franz.zbinden7@gmail.com>
2025-10-13 22:41:45 -0400

    Cleared the i/o of string data in go functions

---
+++ Go/04_io/students_file_io/circle_calc_io/circle_calc_io.go
@@ -0,0 +1,94 @@
+/*
+ * File: circle_calc_io.go
+ * Author: Antonio F. Huertas
+ * Course: COTI 4039-LH1
+ * Date: 09/29/2025
+ * Purpose: This program computes and displays the area and circumference of
+ *          the circle with the given radius.
+ */
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+// Computes the area of the circle with the given radius.
+func circleArea(rad float64) float64 {
+	return math.Pi * math.Pow(rad, 2.0)
+}
+
+// Computes the circumference of the circle with the given radius.
+func circleCircumference(rad float64) float64 {
+	return 2.0 * math.Pi * rad
+}
+
+// Reads a value and validates it is a number.
+func readNumber(prompt string) float64 {
+	for {
+		var value string
+		fmt.Print("\n" + prompt + " ") // This prints to the user the request info
+		fmt.Scanln(&value)             // This fetches the info from the user and insert in the addres of value
+
+		num, err := strconv.ParseFloat(value, 64) // Converts the string into a float 64 -> num
+		if err != nil {                           // If thers an error stored into err, print the error message
+			fmt.Println("\tError! The value is not a number")
+		} else { //else return the floating point number 64
+			return num
+		}
+	}
+}
+
+func read_number(prompt string) float64 {
+	for {
+		var value string
+		fmt.Print("\n" + prompt + " ")
+		fmt.Scanln(&value)
+
+		num, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			fmt.Println("Thers an error with the provided value")
+		} else {
+			return num
+		}
+	}
+}
+
+// Reads a number and validates it is positive.
+func readPositive(prompt string) float64 {
+	num := readNumber(prompt)
+	for num <= 0.0 {
+		fmt.Println("\tError! The value is not a positive number")
+		num = readNumber(prompt)
+	}
+	return num
+}
+
+func read_positive(prompt string) float64 {
+	num := read_number(prompt)
+	if num <= 0 {
+		fmt.Println("Error, value provided must be positive")
+		num = read_number(prompt)
+	}
+	return num
+}
+
+// Starts the execution of the program.
+func main() {
+	radius := readPositive("Enter the radius:")
+	fmt.Printf("The radius of the circle is %.1f\n", radius)
+
+	fmt.Printf("\nIts area is %.1f and its circumference is %.1f\n",
+		circleArea(radius), circleCircumference(radius))
+
+	// ===========================================================
+
+	rad := read_number("Enter the radius of a circle:")
+	fmt.Printf("\nThe radius of the circle is %.2f\n", rad)
+
+	circle_area, circle_circumference := circleArea(rad), circleCircumference(rad)
+	fmt.Printf("The area of the circle is: %.2f, and the circumference is: %.2f\n",
+		circle_area, circle_circumference)
+}
